cfbb523a7114d28117abe2d83d3becb65aeecf72
Alexandre MOREILHON <amoreilh@dump-alex.42.fr>
2017-02-16 11:44:54 +0100

    lol

--- lib/carrierwave/ffmpeg.rb
+++ lib/carrierwave/ffmpeg.rb
@@ -29,7 +29,7 @@ module CarrierWave
         { video_codec: 'libvpx',    
           audio_codec: 'libvorbis' }
       when :ogv
-        { video_codec: 'libvpx', 
+        { video_codec: 'theora', 
           audio_codec: 'libvorbis' }
       else
         raise CarrierWave::ProcessingError.new("Unsupported video format. Error: #{e}")
